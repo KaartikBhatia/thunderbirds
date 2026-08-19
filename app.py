9c41c267f72f242e0c40f193f82dd2bea14dbdf9
Kaartik Bhatia <kaartikbhatia@gmail.com>
2026-08-19 12:53:21 -0400

    Updates

--- app.py
+++ app.py
@@ -2,10 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-
+@app.route('/index.html')
 def welcome():
     return render_template('index.html')
 
+@app.route('/roster.html')
+def roster():
+    return render_template('roster.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+
