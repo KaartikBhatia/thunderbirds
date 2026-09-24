6d0db4cbe3f639c7e9490d92f23d296a75b7aa59
Kaartik Bhatia <kaartikbhatia@gmail.com>
2026-09-24 09:45:14 -0400

    testing

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ def welcome():
 def roster():
     return render_template('roster.html')
 
+@app.route('/about.html')
+def about():
+    return render_template('about.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
 
